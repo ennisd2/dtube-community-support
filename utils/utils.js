1b66dfaa21fa5194a16c88af5e6b779574067efa
evildido <evildido@gmail.com>
2018-03-21 22:48:02 +0100

    makes ifExistInDB exporable

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,7 @@
 const steem = require('steem');
 var config = require('config.json')('./config.json');
+var Store = require("jfs");
+var db = new Store("data");
 
 
 exports.failover = function() {
@@ -23,3 +25,28 @@ exports.failover = function() {
 }
 
 	
+exports.ifExistInDB = function(input,callback) {
+  
+  db.get("metadata_store", function(err, metadata_store){
+    if(!err)
+    {
+      
+      if(metadata_store.some(function(r){return r.pinset===input.pinset})) {
+        console.log(input.pinset + " already stored")
+        exist=true;
+      }
+      else
+      {
+        console.log(input.pinset + " not already stored");
+        exist=false;
+      }
+    }
+    else
+    {
+      //console.log(err);
+      console.log("database empty... continue")
+      exist=false;
+    }
+    callback(null,input,exist);
+  });
+}
