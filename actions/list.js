c793f51933db2d55c37a6b607912cc4896f25a6d
evildido <evildido@gmail.com>
2018-03-21 22:49:18 +0100

    fix list function
    don’t try to display result if DB is empty

--- actions/list.js
+++ actions/list.js
@@ -104,51 +104,96 @@ exports.list = function () {
 			});
 		},
 		function(repoSize, storageMax,callback) {
-			db.get("metadata_store", function(err, metadata){
-				var list = [];
-				if(args.sort!=undefined){
-					switch(args.sort) {
-						case "size" : 
-							metadata = metadata.sort(function(a,b) {
-								return b.size - a.size;
-							});
-							break;
-						case "date" :
-							metadata = metadata.sort(function(a,b) {
-								return new Date(b.date) - new Date(a.date);
-							});
-							break;
-						default:
-							metadata = metadata_store.sort(function(a,b) {
-								return new Date(a.date) - new Date(b.date);
-							})
-
+			db.get("metadata_store", function(err, metadata_store){
+				if(err) callback(true);
+				else
+				{
+					var list = [];
+					if(args.sort!=undefined){
+						switch(args.sort) {
+							case "size" : 
+								metadata_store = metadata_store.sort(function(a,b) {
+									return b.size - a.size;
+								});
+								break;
+							case "date" :
+								metadata_store = metadata_store.sort(function(a,b) {
+									return new Date(b.date) - new Date(a.date);
+								});
+								break;
+							default:
+								metadata_store = metadata_store.sort(function(a,b) {
+									return new Date(a.date) - new Date(b.date);
+								})
+	
+						}
+	
 					}
-
+	
+					if(args.author!=undefined) {
+						result = filterByAuthor(args.author, metadata_store);
+						authorSize=result.authorSize;
+						metadata_store=result.metadata;
+						console.log("User " + args.author + " use : " + Number(authorSize/1000000).toFixed(2) + "Mo");
+					}
+	
+					console.log("Repository usage : " + Number(repoSize/1000000000).toFixed(2) + "Go / " +Number(storageMax/1000000000).toFixed(2) + "Go");
+					metadata_store.forEach((result)=>{
+						tmp = {};
+						tmp.pinset=result.pinset;
+						tmp.date=new Date(result.date);
+						tmp.size = Number(result.size/1000000).toFixed(2) + "Mo";
+						tmp.link = result.link;
+						list.push(tmp);
+					});
+	
+					console.log(columnify(list,{columnSplitter: ' | '}));
 				}
 
-				if(args.author!=undefined) {
-					result = filterByAuthor(args.author, metadata);
-					authorSize=result.authorSize;
-					metadata=result.metadata;
-					console.log("User " + args.author + " use : " + Number(authorSize/1000000).toFixed(2) + "Mo");
-				}
-
-				console.log("Repository usage : " + Number(repoSize/1000000000).toFixed(2) + "Go / " +Number(storageMax/1000000000).toFixed(2) + "Go");
-				metadata.forEach((result)=>{
-					tmp = {};
-					tmp.pinset=result.pinset;
-					tmp.date=new Date(result.date);
-					tmp.size = Number(result.size/1000000).toFixed(2) + "Mo";
-					tmp.link = result.link;
-					list.push(tmp);
-				});
-
-				console.log(columnify(list,{columnSplitter: ' | '}));
 			});
 
 		}
-		])
+	])
+}
 
-	
+exports.report = function () {
+	async.waterfall(
+		[
+		function(callback){
+
+			ipfs.repo.stat((err,stats) => {
+				callback(null,stats.repoSize,stats.storageMax);
+			});
+		},
+		function(repoSize, storageMax,callback) {
+			db.get("metadata_store", function(err, metadata){
+				var list = [];
+
+				metadata = metadata.sort(function(a,b) {
+					return new Date(a.date) - new Date(b.date);
+				})
+
+
+
+				header = "| Title | author | Date | size |";
+				format = "| ------ | -------- | :---: | ----: |";
+				console.log(header);
+				console.log(format);
+				list.push(header);
+				list.push(format);
+				metadata.forEach((result)=>{
+					tmp = {};
+					date = new Date(result.date);
+					options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+					date = date.toLocaleDateString();
+					title = result.title;
+					title = title.replace("|","&#124;");
+					title = title.length > 50 ? title.substring(0, 20) + "..." : title
+					title = "[" + title + "](https://d.tube" + result.link + ")";
+					console.log("| " + title + "| " + "@" +  result.author + " | " + date + " | " + Number(result.size/1000000).toFixed(2) + "Mo" + " | ")
+				});
+			});
+
+		}
+	]);
 }
